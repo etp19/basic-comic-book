e864965cc850624b958fa1b0881181d2f10cc516
Eduardo Torres Perez <torrespe@mail.lcc.edu>
2017-11-01 23:21:40 -0400

    Added Comic Book Detail view.

--- src/ComicBookGallery/Controllers/HomeController.cs
+++ src/ComicBookGallery/Controllers/HomeController.cs
@@ -9,9 +9,9 @@ namespace ComicBookGallery.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello, This is the home page";
+            return Content("Hello, This is the home page");
         }
     }
 }
